fd2f4e204b7e7acce16c321d6d2a952fbd19e643
softontechsolution <softontechsolution@gmail.com>
2024-05-19 07:19:14 -0700

    Layout Created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: TaskViewComponent }
+];
